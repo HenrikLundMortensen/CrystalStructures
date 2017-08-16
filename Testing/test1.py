e6fcef42a22c027bfdcbced756f84264a31f9c54
SoerenMeldgaard <sm@phys.au.dk>
2017-08-16 16:28:30 +0200

    Fixing

--- Testing/test1.py
+++
@@ -1,8 +0,0 @@
-import crystalStructures.energyCalculations.energyLennardJones as lj
-import crystalStructures.coordinateSet as cs
-import scipy.optimize.basinhopping as bh
-
-myCoordinateSet = cs.CoordinateSet()
-myCoordinateSet.createRandomSet(5)
-
-params = [1, 1, 1]
