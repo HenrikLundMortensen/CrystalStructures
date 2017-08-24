5a97174f407678257739c53ab029d7338a4dd863
SoerenMeldgaard <sm@phys.au.dk>
2017-08-24 12:37:19 +0200

    Updating clusters to handle list of feature vectors

--- clustering/clusterHandler.py
+++ clustering/clusterHandler.py
@@ -5,11 +5,11 @@ import numpy as np
 class ClusterHandler:
     """
     Takes a CoordinateSet class with calculated feature vectors
-    Then cluster the feature vectors into a given number of clusters 
+    Then cluster the feature vectors into a given number of clusters
     and returns the KMeans
 
     #### Attributes ####
-    CoordinateSet: in instance of the CoordinateSet class 
+    CoordinateSet: in instance of the CoordinateSet class
     K: the number of clusters
     FeatureVectors: the feature vectors one wish to cluster
     KMeans: an instance of KMeans from sklearn.cluster
@@ -19,10 +19,11 @@ class ClusterHandler:
     doClustering: cluster the given coordinateSet
     """
 
-    def __init__(self, coordinateSet, clusters):
+    def __init__(self, clusters, coordinateSet=None):
         self.CoordinateSet = coordinateSet
         self.K = clusters
-        self.FeatureVectors = coordinateSet.FeatureVectors
+        if coordinateSet:
+            self.FeatureVectors = coordinateSet.FeatureVectors
 
     def doClustering(self):
         if isinstance(self.FeatureVectors[0], int):  # Special for 1d feature vectors
@@ -30,4 +31,17 @@ class ClusterHandler:
         else:
             featVec = np.vstack(self.FeatureVectors)  # Convert into numpy array
             self.Kmeans = KMeans(n_clusters=self.K).fit(featVec)
+
+    def doClusteringList(self, featureVectorList):
+        if isinstance(featureVectorList[0], int):  # Special for 1d feature vectors
+            self.Kmeans = KMeans(n_clusters=self.K).fit(np.asarray(self.FeatureVectorList).reshape(-1, 1))
+        else:
+            self.Kmeans = KMeans(n_clusters=self.K).fit(featureVectorList)
+
+
+if __name__ == '__main__':
+    FeatureVectorList = np.array(([0, 0, 0], [1, 1, 1], [2, 2, 2]))
+    myClusterHandler = ClusterHandler(3)
+    myClusterHandler.doClusteringList(FeatureVectorList)
+    print(myClusterHandler.Kmeans.labels_)
     
