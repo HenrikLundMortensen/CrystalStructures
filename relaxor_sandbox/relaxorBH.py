c4a8d1f43dc8b324bb81df4e5d1b67f1ca1d4d90
MaltheBisbo <30654168+MaltheBisbo@users.noreply.github.com>
2017-08-24 14:34:03 +0200

    updated stepper in relaxorBH

--- relaxor_sandbox/relaxorBH.py
+++ relaxor_sandbox/relaxorBH.py
@@ -60,6 +60,31 @@ class takeStep(object):
         return x
 
 
+class takeStep2(object):
+    def __init__(self, boxsize, stepsize=0.1):
+        self.boxSize = boxSize
+        self.stepsize = stepsize
+        
+    def __call__(self, x):
+        N = np.size(x, 0)
+        # force atoms inside the box
+        x = np.clip(x, 0, self.boxSize)
+        
+        dx = np.random.uniform(-self.stepsize, self.stepsize, N)
+        xnew = np.clip(x + dx, 0, self.boxSize)
+        above = bool(np.any(xnew >= self.boxSize))
+        below = bool(np.any(xnew <= 0))
+        if above or below:
+            self.stepsize *= 0.7
+            xnew_above = (xnew > self.boxSize).astype(int)
+            xnew_below = (xnew < 0).astype(int)
+            xdiff = xnew - x
+            x = xnew - xdiff*xnew_above / 2 - xdiff*xnew_below / 2
+        else:
+            x = np.copy(xnew)
+        return x
+
+    
 def print_fun(x, f, accepted):
     print("at minimum %.4f accepted %d" % (f, int(accepted)))
 
@@ -93,33 +118,59 @@ class relaxor:
         print("at minimum %.4f accepted %d" % (f, int(accepted)))
         
     def runRelaxor(self):
-        myTakeStep = takeStep(self.boxSize)
+        myTakeStep = takeStep2(self.boxSize, 2)
         minimizer_kwargs = {"args": self.params, "jac": True}
         t0 = time.time()
         self.res = basinhopping(self.Efunc, self.x,
+                                T=1,
+                                interval=1,
                                 niter=300,
                                 take_step=myTakeStep,
-                                niter_success=5,
+                                niter_success=40,
                                 minimizer_kwargs=minimizer_kwargs,
                                 callback=print_fun)
         self.runtime = time.time() - t0
 
 if __name__ == "__main__":
 
-    # Make random atom configuration
-    N = 10
-    boxSize = 6
-    x = np.random.rand(N, 2) * boxSize  # for plotting
-    x0 = np.reshape(x, N * 2)  # Reshape for basinhopping
+    N = 30
     
     # Define parameters for energy expression
-    eps = 1.8  # 1.8
-    r0 = 1.1  # 1.1
+    eps = 1  # 1.8
+    r0 = 1.4  # 1.1
     sigma = np.sqrt(0.02)
     params = (eps, r0, sigma)
 
+    # set boxsize
+    boxSize = 1.5 * np.sqrt(N)*r0
+    
+    # Make random atom configuration
+    x = np.random.rand(N, 2) * boxSize  # for plotting
+    x0 = np.reshape(x, N * 2)  # Reshape for basinhopping
+    
     # Run and time basinhopping
     relax = relaxor(x, E_LJ_jac, params, boxSize)
     relax.runRelaxor()
     xres = relax.res.x
     print("\nRuntime:", relax.runtime)
+    print("Eopt:", relax.res.fun)
+
+    """
+    stepper = takeStep2(6, 2)
+    x1 = np.copy(x0)
+    x2 = stepper(x0)
+    x1 = np.reshape(x1, (N, 2))
+    x2 = np.reshape(x2, (N, 2))
+    """
+    
+    import matplotlib.pyplot as plt
+    plt.figure(1)
+    Xbox = [0, boxSize, boxSize, 0, 0]
+    Ybox = [0, 0, boxSize, boxSize, 0]
+    plt.plot(Xbox, Ybox, color='black')
+
+    # Plot atoms
+    xres = np.reshape(xres, (N, 2))
+    plt.plot(xres[:, 0], xres[:, 1], 'o', color='red', ms=2)
+    plt.show()
+    
